cbc65894952cc7236d6cebe1269fa68ddc650786
Alexey Pankov <alexey.v.pankov@gmail.com>
2015-10-06 22:33:59 +0300

    next update

--- mod_php5_apache2/attributes/customize.rb
+++ mod_php5_apache2/attributes/customize.rb
@@ -1,18 +1,17 @@
 packages = [
     "php55-common",
-    "php-process",
+    "php55-process",
     "php-xsl",
     "php-curl",
-    "php-xmlrpc",
-    "php-devel",
-    "php-gd",
-    "php-mbstring",
-    "php-cli",
-    "php-mcrypt",
-    "php-xml",
-    "php-opcache",
-    "php-pear",
-    "php-xml-parser"
+    "php55-xmlrpc",
+    "php55-devel",
+    "php55-gd",
+    "php55-mbstring",
+    "php55-cli",
+    "php55-mcrypt",
+    "php55-xml",
+    "php55-opcache",
+    "php-pear"
   ]
 
 normal[:mod_php5_apache2][:packages] = packages
