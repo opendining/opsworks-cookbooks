7dbf0b7e0fb8ded500f4a3c5e10f3a9567f39967
Alexey Pankov <alexey.v.pankov@gmail.com>
2015-10-07 17:58:22 +0300

    added odn setup

---
+++ odn/recipes/setup.rb
@@ -0,0 +1,19 @@
+Chef::Log.info("******Installing MongoClient******")
+
+execute "no | pecl install mongo" do
+  user "root"
+  ignore_failure true
+end
+
+execute "echo 'extension=mongo.so' | sudo tee -a /etc/php5/apache2/conf.d/20-mongo.ini" do
+  user "root"
+  ignore_failure true
+end
+
+service 'apache2' do
+  service_name value_for_platform_family(
+    'rhel' => 'httpd',
+    'debian' => 'apache2'
+  )
+  action :restart
+end
