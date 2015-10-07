bc9e50482fb3ab3bc03985d7a8ea55ce23b079dc
Alexey Pankov <alexey.v.pankov@gmail.com>
2015-10-07 12:13:04 +0300

    added ODN recipe

---
+++ odn/metadata.rb
@@ -0,0 +1,2 @@
+name "odn"
+version "0.1.0"
---
+++ odn/recipes/deploy.rb
@@ -0,0 +1,20 @@
+node[:deploy].each do |application, deploy|
+
+  current_dir = ::File.join(deploy[:deploy_to], 'current')
+
+  Chef::Log.info("******Changing permissions******")
+  Chef::Log.info(current_dir)
+
+  execute "chmod -R 777 admin/application/cache/" do
+    user "root"
+    cwd current_dir
+    ignore_failure true
+  end
+
+  execute "chmod -R 777 admin/application/logs/" do
+    user "root"
+    cwd current_dir
+    ignore_failure true
+  end
+
+end
