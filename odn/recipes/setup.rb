96f19c80bea7a2d7dbba54f6797d2ea64c6c9efd
Alexey Pankov <alexey.v.pankov@gmail.com>
2015-10-20 16:20:08 +0300

    enabled mcrypt module

--- odn/recipes/setup.rb
+++ odn/recipes/setup.rb
@@ -15,6 +15,11 @@ execute "echo 'extension=mongo.so' | sudo tee -a /etc/php5/apache2/conf.d/20-mon
   ignore_failure true
 end
 
+execute "php5enmod mcrypt" do
+  user "root"
+  ignore_failure true
+end
+
 execute "echo 'max_input_vars = 5000\nmemory_limit = 256M\nupload_max_filesize = 20M\npost_max_size = 20M\nshort_open_tag = On\n' | sudo tee -a /etc/php5/apache2/conf.d/20-php.ini" do
   user "root"
   ignore_failure true
