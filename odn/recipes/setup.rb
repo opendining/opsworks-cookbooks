de17b4b630a885e826f5e345c1b89f664972ef50
Alexey Pankov <alexey.v.pankov@gmail.com>
2017-03-13 21:18:30 +0300

    increased php memory limit

--- odn/recipes/setup.rb
+++ odn/recipes/setup.rb
@@ -25,7 +25,7 @@ execute "php5enmod mysqli" do
   ignore_failure true
 end
 
-execute "echo 'max_input_vars = 5000\nmemory_limit = 256M\nupload_max_filesize = 20M\npost_max_size = 20M\nshort_open_tag = On\n' | sudo tee -a /etc/php5/apache2/conf.d/20-php.ini" do
+execute "echo 'max_input_vars = 5000\nmemory_limit = 512M\nupload_max_filesize = 20M\npost_max_size = 20M\nshort_open_tag = On\n' | sudo tee -a /etc/php5/apache2/conf.d/20-php.ini" do
   user "root"
   ignore_failure true
 end
